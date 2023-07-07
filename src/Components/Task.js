a3d3ac17df8559edc16c77f764f00224b1dec229
itsVESAL <itvesal@gmail.com>
2023-07-08 01:24:01 +0330

    final commit

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 
+//icon
+import editIcon from "../assets/Icons/editIcon.svg";
+import tickIcon from "../assets/Icons/tick.svg";
+
 //Styles
 import styles from "./Styles/Task.module.css";
 
 const Task = ({ taskData }) => {
   const [checkBoxValue, setCheckBoxValue] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editValue, setEditValue] = useState(taskData.task);
 
   const changeHandler = (e) => {
     setCheckBoxValue(e.target.checked);
@@ -12,14 +18,52 @@ const Task = ({ taskData }) => {
     // console.log(taskData);
   };
 
+  const editHandler = () => {
+    taskData.isEditing = true;
+    setIsEditing(taskData.isEditing);
+  };
+
+  const clickHandler = () => {
+    taskData.isEditing = false;
+    setIsEditing(taskData.isEditing);
+    taskData.task = editValue;
+  };
+
   return (
     <div className={styles.container}>
+      
       <input type="checkbox" value={checkBoxValue} onChange={changeHandler} />
-      {taskData.completed ? (
-        <p className={styles.completed}>{taskData.task}</p>
-      ) : (
-        <p className={styles.uncompleted}>{taskData.task}</p>
-      )}
+
+      {taskData.completed ? 
+        <p className={styles.completed}>{taskData.task} </p>
+       : 
+        <div className={styles.taskContainer}>
+          {isEditing ? 
+            <>
+              <input
+                value={editValue}
+                onChange={(e) => {
+                  setEditValue(e.target.value);
+                }}
+              />
+              <img
+                className={styles.icon}
+                src={tickIcon}
+                onClick={clickHandler}
+              />
+            </>
+           : 
+            <>
+              <p>{taskData.task}</p>
+              <img
+                className={styles.icon}
+                src={editIcon}
+                onClick={editHandler}
+              />
+            </>
+          }
+        </div>
+      }
     </div>
   );
 };
